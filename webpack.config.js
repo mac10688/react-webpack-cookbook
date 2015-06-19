fc1ae4f9ad33f915c48f03fdb1433f1a3da4b5e6
mac10688 <mac10688@gmail.com>
2015-06-18 21:03:39 -0500

    Adding files

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+var path = require('path');
+
+
+module.exports = {
+    entry: "./entry.js",
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+    },
+};
