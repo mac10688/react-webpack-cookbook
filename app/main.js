8626edd0a19c7f729ebeaaef71150ced1f519ffc
mac10688 <mac10688@gmail.com>
2015-06-18 22:59:45 -0500

    Added automatic refresher

--- app/main.js
+++ app/main.js
@@ -1,5 +1,9 @@
 'use strict';
 var component = require('./component.js');
 
+import MyModule from './modules/MyModule.js';
+
+var MyModule = require('./modules/MyModule.js');
+
 
 document.body.appendChild(component());
---
+++ app/modules/MyModule.js
@@ -0,0 +1,7 @@
+import utils from './../utils.js';
+
+import utils from '/utils.js';
+
+var utils = require('./../utils.js');
+
+var utils = require('/utils.js');
